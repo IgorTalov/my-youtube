6e3c3e3d79b2551b941baeb88418c54727fed20b
Игорь Талов <igortalov@gmail.com>
2017-03-26 22:36:57 +0300

    Initial Commit

---
+++ youTubeTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  youTubeTest
+//
+//  Created by Игорь Талов on 26.03.17.
+//  Copyright © 2017 Игорь Талов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
